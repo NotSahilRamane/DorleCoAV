c64633ec2b02eeec58ec01a453187debbdc6db01
notsahilramane <sahil.ramane@somaiya.edu>
2022-06-29 11:43:11 +0530

    Revert "Added global planner"
    This reverts commit d9453bd4dd249a8b08bd4e2f008b14e5b6775eaa.

--- src/planning/global_planner/scripts/planner.py
+++ src/planning/global_planner/scripts/planner.py
@@ -7,53 +7,5 @@ import time
 import math
 from ros_numpy import occupancy_grid
 
-from av_messages.msg import carState, globalPlan, behaviour, localPlan, wayPoint, destination
+from av_messages.msg import carState, globalPlan, behaviour, localPlan, wayPoint
 from nav_msgs.msg import OccupancyGrid
-from graph_msgs.msg import GeometryGraph
-
-class GlobalPlanner:
-    def __init__(self):
-        self.loadParameters()
-    
-    def subscribeToTopics(self):
-        rospy.loginfo("Subscribed to topics")
-        rospy.Subscriber(self.destination_topic_name, destination, buff_size=2**24, queue_size=1, callback=self.destinationCallback)
-        rospy.Subscriber(self.current_state_topic_name, carState, callback=self.stateCallback)
-        rospy.Subscriber(self.initial_state_topic_name, carState, callback=self.initialStateCallback)
-        rospy.Subscriber(self.global_map_topic_name, GeometryGraph, callback=self.graphMapCallback)
-
-    def loadParameters(self):
-        '''
-        add data
-        '''
-
-    def publishToTopics(self):
-        rospy.loginfo("Published to topics")
-        self.waypointsPublisher = rospy.Publisher(
-            self.pub_topic_name, globalPlan, queue_size=1)
-
-    def stateCallback(self, data):
-        self.carPos = data.car_state
-        self.carVel = data.car_state_dt
-
-    def initialStateCallback(self, data):
-        self.initialCarPos = data.car_state
-        self.initialCarVel = data.car_state_dt
-
-    def destinationCallback(self, data):
-        self.destination = (data.latitude, data.longitude)
-    
-    def graphMapCallback(self, data):
-        self.map_intersections = data.nodes
-        self.map_roads = data.edges
-        self.callPlanner()
-
-    def callPlanner(self):
-        '''
-        Call the global planner algorithm here
-        '''
-
-    def callPublisher(self):
-        '''
-        Call the final publisher here
-        '''
