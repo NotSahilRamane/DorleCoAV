8d895519dfaea137ad55adad5f8c32e2376c11a0
Reuben <reubenthomas.john2017@gmail.com>
2022-07-08 18:52:17 +0530

    corrected ros node files

--- src/perception/road_segmentation/scripts/road_segmentation.py
+++ src/perception/road_segmentation/scripts/road_segmentation.py
@@ -5,9 +5,10 @@ import rospy
 
 from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image
-from av_messages.msg import depthandimage
+# from av_messages.msg import depthandimage
 
-from YOLOP.tools import YOLO_P
+from YOLOP.tools.yolo_p import YOLOP_Class
+
 
 import numpy as np
 import math
@@ -18,7 +19,7 @@ class Detector:
         self.bridge = CvBridge()
         self.rgb_image = None
         self.depth_image = None
-        self.yolo_p = YOLO_P()
+        self.yolo_p = YOLOP_Class()
         
     # def subscribeToTopics(self):
     #     rospy.loginfo("Subscribed to topics")
@@ -42,13 +43,13 @@ class Detector:
     def publishToTopics(self):
         rospy.loginfo("Published to topics")
         self.DetectionsPublisher = rospy.Publisher(
-            self.pub_topic_name, "Will have to think about this", queue_size=1)
+            self.pub_topic_name, Image, queue_size=1)
 
     def storeImage(self, img):
         try:
             self.rgb_image = self.bridge.imgmsg_to_cv2(img, 'bgr8')
             # self.depth_image = self.bridge.imgmsg_to_cv2(img.depth_image, "32FC1") ## Confirm these once
-            self.callDetector()
+            self.callSegmentationModel()
         except CvBridgeError as e:
             rospy.loginfo(str(e))
     
@@ -60,9 +61,9 @@ class Detector:
         img_det, _, _ = self.yolo_p.detect(self.rgb_image)
         self.callPublisher(img_det)
 
-    def callPublisher(self):
+    def callPublisher(self, image):
         '''
         the final publisher function
         '''
-        segmented_image = self.bridge.cv2_to_image()
+        segmented_image = self.bridge.cv2_to_imgmsg(image, 'bgr8')
         self.DetectionsPublisher.publish(segmented_image)
