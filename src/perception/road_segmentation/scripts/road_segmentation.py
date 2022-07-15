79774d16536cb8f303db467e4588f9a57e92e705
notsahilramane <sahil.ramane@somaiya.edu>
2022-07-15 12:54:30 +0530

    Added depth subscriber and synced frames

--- src/perception/road_segmentation/scripts/road_segmentation.py
+++ src/perception/road_segmentation/scripts/road_segmentation.py
@@ -21,6 +21,8 @@ class Detector:
         self.rgb_image = None
         self.depth_image = None
         self.yolo_p = YOLOP_Class()
+        self.RGB_IMAGE_RECEIVED = 0
+        self.DEPTH_IMAGE_RECEIVED = 0
         
     # def subscribeToTopics(self):
     #     rospy.loginfo("Subscribed to topics")
@@ -31,15 +33,19 @@ class Detector:
         rospy.loginfo("Subscribed to topics")
         rospy.Subscriber(self.image_topicname, Image,
                          self.storeImage, queue_size=1)
+        rospy.Subscriber(self.depth_image_topicname, Image,
+                         self.storeDepthImage, queue_size=1)
 
     def loadParameters(self):
         '''
         do something
         '''
         self.image_topicname = rospy.get_param(
-            "camera_object_detector/image_topic_name", "/carla/ego_vehicle/rgb_view/image")
+            "road_segmentation/image_topic_name", "/carla/ego_vehicle/rgb_front/image")
+        self.depth_image_topicname = rospy.get_param(
+            "road_segmentation/depth_image_topic_name", "/carla/ego_vehicle/depth_front/image")
         self.pub_topic_name = rospy.get_param(
-            "lane_detector/road_segmentation_topic_name", "/camera/roadsegmentation")
+            "road_segmentation/road_segmentation_topic_name", "/camera/roadsegmentation")
     
     def publishToTopics(self):
         rospy.loginfo("Published to topics")
@@ -47,14 +53,35 @@ class Detector:
             self.pub_topic_name, Image, queue_size=1)
 
     def storeImage(self, img):
-        try:
-            self.rgb_image = self.bridge.imgmsg_to_cv2(img, 'bgr8')
-            # self.depth_image = self.bridge.imgmsg_to_cv2(img.depth_image, "32FC1") ## Confirm these once
-            self.callSegmentationModel(self.rgb_image)
-        except CvBridgeError as e:
-            rospy.loginfo(str(e))
+        if self.RGB_IMAGE_RECEIVED == 0:
+            try:
+                frame = self.bridge.imgmsg_to_cv2(img, 'bgr8')
+                rospy.loginfo("RGB Image Stored")
+            except CvBridgeError as e:
+                rospy.loginfo(str(e))
+            self.rgb_image = frame
+            self.RGB_IMAGE_RECEIVED = 1
+            self.sync_frames()
+
+    def storeDepthImage(self, img):
+        frame=None
+        if self.DEPTH_IMAGE_RECEIVED == 0:
+            try:
+                frame = self.bridge.imgmsg_to_cv2(img, "32FC1")
+                rospy.loginfo("Depth Image Stored")
+            except CvBridgeError as e:
+                rospy.loginfo(str(e))
+            self.depth_image = frame
+            self.DEPTH_IMAGE_RECEIVED = 1
+            self.sync_frames()
+
+    def sync_frames(self):
+        if self.RGB_IMAGE_RECEIVED == 1 and self.DEPTH_IMAGE_RECEIVED == 1:
+            self.callSegmentationModel(self.rgb_image, self.depth_image)
+            self.DEPTH_IMAGE_RECEIVED = 0
+            self.RGB_IMAGE_RECEIVED = 0
     
-    def callSegmentationModel(self, image):
+    def callSegmentationModel(self, image, depth_image):
         '''
         Call the segmentation model related functions here (Reuben, Mayur)
         and the final publish function (To be done by sahil)
