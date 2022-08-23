a4bc8eb7bb4df69c5ed238820cf37c174f00d582
Mayakshanesht <mayakshanesht1995@gmail.com>
2022-08-23 16:34:09 +0530

    updated av messages

--- src/lateral_control/scripts/test.py
+++ src/lateral_control/scripts/test.py
@@ -1,5 +1,6 @@
-import numpy
-list=[]*5
-# for i in range(1,5):
-#     list.append(i)
-print(list)
+import numpy as np
+a=[100, 100,200,300,400,400]
+a.sort()
+for i in range(1,len(a)):
+    if (a[i-1]==a[i]):
+        print(a[i])
